49152724d60ac2049e8c152bc56fcb22bd12668b
priyanka1551 <priyankashar717@gmail.com>
2023-07-06 11:52:03 +0530

    issues in index.js resolved

--- index.js
+++ index.js
@@ -14,9 +14,8 @@ app.use(cors());
 
 app.use('/posts', postRoutes);
 app.use("/user", userRouter);
-
-const CONNECTION_URL = 'mongodb+srv://priyankashar717:FdvXmZXgrZJLEvMZ@memoriescluster.sbeynhh.mongodb.net/';
-const PORT = process.env.PORT|| 5000;
+const PORT = process.env.PORT || 5000;
+const CONNECTION_URL=process.env.CONNECTION_URL;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
